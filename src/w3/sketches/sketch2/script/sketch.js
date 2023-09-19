303e45531f329501b4787c507b266eb49fa33d9d
parksion0629 <143991677+parksion0629@users.noreply.github.com>
2023-09-19 11:07:33 +0900

    3주차 코드 수정

--- src/w3/sketches/sketch2/script/sketch.js
+++ src/w3/sketches/sketch2/script/sketch.js
@@ -4,24 +4,27 @@ let acc;
 let radius = 50;
 
 function setup() {
-  // setCanvasContainer('mySketchGoesHere', 3, 2, true);
+  setCanvasContainer('mySketchGoesHere', 3, 2, true);
   background(255);
-  // pos = createVactor(width / 2, height / 2);
-  // vel = createVactor(0, 0);
-  // acc = createVactor(0, 1);
-  // console.log(pos);
-  // console.log(vel);
-  vel.add(acc);
-  pos.add(vel);
+  pos = createVactor(width / 2, height / 2);
+  vel = createVactor(0, 0);
+  acc = createVactor();
+  console.log(pos);
+  console.log(vel);
+  // vel.add(acc);
+  // pos.add(vel);
   ellipse(pos.x, pos.y, 50);
 }
 
 function draw() {
   background(255);
-  acc = p5.Vector.random2D();
-  acc.mult(0.5);
-  vel.limit(5);
-  pos.add(vel);
+  update();
+  InfiniteEdge();
+  display();
+  // acc = p5.Vector.random2D();
+  // acc.mult(0.5);
+  // vel.limit(5);
+  // pos.add(vel);
   // if (pos.x < 0) {
   //   vel.x *= -1;
   // } else if (pos.x > width) {
@@ -33,5 +36,18 @@ function draw() {
   // if (pos.y - radius < 0 || pos.y + radius > height) {
   //   vel.y *= -1;
   // }
-  if (pos.x < o) ellipse(pos.x, pos.y, 2 * radius);
+  // if (pos.x < o) ellipse(pos.x, pos.y, 2 * radius);
+}
+
+function display() {
+  Fill('red');
+  ellipse(pos.x, pos.y, 2 * radius);
+}
+
+function update() {
+  acc = p5.Vector.random2D();
+  acc.mult(0.5);
+  vel.add(acc);
+  vel.limit(5);
+  pos.add(vel);
 }
