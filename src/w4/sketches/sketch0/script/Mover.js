dc5dd7d7ec458971a1e3c98432f2c09cf14893e2
parksion0629 <143991677+parksion0629@users.noreply.github.com>
2023-10-01 02:42:35 +0900

    4w 1

--- src/w4/sketches/sketch0/script/Mover.js
+++ src/w4/sketches/sketch0/script/Mover.js
@@ -1,43 +1,153 @@
-class Moverb {
-  constructor(x, y, radius) {
+class Mover {
+  constructor(x, y, r) {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.radius = radius;
-    this.mass = radius ** (1 / 2);
-  }
-
-  applyForce(force) {
-    // force.div(this.mass);
-    let divedForce = p5.Vector.div(force, this.mass);
-    this.acc.add(divedForce);
+    this.acc = createVector(0, 0.1);
+    this.radius = r;
   }
 
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.muit(0);
-    //가속도를 초기화 해둔자는 뜻
   }
 
-  edgeBounce() {
-    if (this.pos.x < 0 + this.radius) {
-      let delta = this.pos.x - (0 + this.radius);
-      this.pos.x += -2 * delta;
+  checkEdges() {
+    if (this.pos.x < 0) {
+      //   // 0보다 얼마나 뚫고 갔는가?
+      //   let delta = this.pos.x - 0;
+      //   // 그 뚫고간 거리에 -1을 곱해 방향을 뒤집고,
+      //   delta *= -1;
+      //   // 0을 기준으로 뒤집힌 거리를 더해준다.
+      //   this.pos.x = 0 + delta;
+      this.pos.x -= 0;
+      this.pos.x *= -1;
+      this.pos.x += 0;
       this.vel.x *= -1;
-    } else if (this.pos.x > width - 1 - this.radius) {
-      let delta = this.pos.x - (width - 1 - this.radius);
-      this.pos.x += -2 * delta;
+    } else if (this.pos.x > width - 1) {
+      this.pos.x -= width - 1;
+      this.pos.x *= -1;
+      this.pos.x += width - 1;
       this.vel.x *= -1;
     }
-    if (this.pos.y > width - 1 - this.radius) {
-      let delta = this.pos.y - (height - 1 - this.radius);
-      this.pos.y += -2 * delta;
+    if (this.pos.y > height - 1) {
+      this.pos.y -= height - 1;
+      this.pos.y *= -1;
+      this.pos.y += height - 1;
       this.vel.y *= -1;
     }
   }
 
   display() {
+    noStroke();
+    fill(0);
     ellipse(this.pos.x, this.pos.y, 2 * this.radius);
   }
+
+  displayVectors() {
+    stroke('red');
+    line(
+      this.pos.x,
+      this.pos.y,
+      this.pos.x + this.vel.x * 10,
+      this.pos.y + this.vel.y * 10
+    );
+    stroke('blue');
+    line(
+      this.pos.x,
+      this.pos.y,
+      this.pos.x + this.acc.x * 100,
+      this.pos.y + this.acc.y * 100
+    );
+  }
 }
+
+// class Mover {
+//   constructor(x, y, r) {
+//     this.pos = createVector(x, y);
+//     this.vel = createVector(0, 0);
+//     this.acc = createVector(0, 0.1);
+//     this.radius = r;
+//     // this.mass = radius ** (1 / 2);
+//   }
+
+//   // applyForce(force) {
+//   //   // force.div(this.mass);
+//   //   let divedForce = p5.Vector.div(force, this.mass);
+//   //   this.acc.add(divedForce);
+//   // }
+
+//   update() {
+//     this.vel.add(this.acc);
+//     this.pos.add(this.vel);
+//     // this.acc.muit(0);
+//     //가속도를 초기화 해둔자는 뜻
+//   }
+
+//   // edgeBounce() {
+//   //   if (this.pos.x < 0 + this.radius) {
+//   //     let delta = this.pos.x - (0 + this.radius);
+//   //     this.pos.x += -2 * delta;
+//   //     this.vel.x *= -1;
+//   //   } else if (this.pos.x > width - 1 - this.radius) {
+//   //     let delta = this.pos.x - (width - 1 - this.radius);
+//   //     this.pos.x += -2 * delta;
+//   //     this.vel.x *= -1;
+//   //   }
+//   //   if (this.pos.y > width - 1 - this.radius) {
+//   //     let delta = this.pos.y - (height - 1 - this.radius);
+//   //     this.pos.y += -2 * delta;
+//   //     this.vel.y *= -1;
+//   //   }
+//   // }
+
+//   checkEdges() {
+//     if (this.pos.x < 0) {
+//       // //벽에 부딪혔을 때 0보다 얼마난 뚫고 갔는가?
+//       // let delta = this.pos.x - 0;
+//       // //뚫고 간 거리에 -1을 곱해 방향을 뒤집고,
+//       // delta *= -1;
+//       // //그리고 0을 기준으로 뒤집힌 거리를 더해준다.
+//       // this.pos.x = 0 + delta;
+//       // this.vel.x *= 1;
+//       this.pos.x -= 0;
+//       this.pos.x *= -1;
+//       this.pos.x += 0;
+//       this.vel.x *= -1;
+//     } else if (this.pos.x > width - 1) {
+//       this.pos.x -= width - 1;
+//       this.pos.x *= -1;
+//       this.pos.x += width - 1;
+//       this.vel.x *= -1;
+//     }
+//     if (this.pos.y > height - 1) {
+//       this.pos.y -= height - 1;
+//       this.pos.y *= -1;
+//       this.pos.y += height - 1;
+//       this.vel.y *= -1;
+//     }
+//   }
+
+//   display() {
+//     noStroke();
+//     fill(0);
+//     ellipse(this.pos.x, this.pos.y, 2 * this.radius);
+//   }
+
+//   displayVector() {
+//     stroke('red');
+//     line(
+//       this.pos.x,
+//       this.pos.y,
+//       this.pos.x + this.vell.x * 10,
+//       this.pos.y + this.vell.y * 10
+//     );
+
+//     stroke('blue');
+//     line(
+//       this.pos.x,
+//       this.pos.y,
+//       this.pos.x + this.acc.x * 100,
+//       this.pos.y + this.acc.y * 100
+//     );
+//   }
+// }
--- src/w4/sketches/sketch0/script/sketch0.js
+++ src/w4/sketches/sketch0/script/sketch0.js
@@ -1,33 +1,33 @@
-let ball;
-let ball2;
-let gravity;
-let wind;
+// let mover;
+
+// function setup() {
+//   setCanvasContainer('canvas', 3, 2, true);
+//   background(255);
+//   mover = new Mover(width / 2, height / 2, 50);
+// }
+
+// function draw() {
+//   background(255);
+//   mover.update();
+//   mover.checkEdges();
+//   mover.display();
+//   mover.displayVector();
+// }
+
+//모든 물체는 중력이 같은 게 아니라 중력 가속도가 같은 거다.
+
+let mover;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  ball = new Moverb(width / 3, 0, 50);
-  ball2 = new Moverb((2 * width) / 3, 0, 10);
-  gravity = createVector(0, 0.1);
-  wind = createVector(-1, 0);
+  background(255);
+  mover = new Mover(width / 2, height / 2, 50);
 }
-
 function draw() {
-  let g = p5.Vector.mult(gravity, ball.mass);
-  ball.applyForce(gravity);
-  let g2 = p5.Vector.mult(gravity, ball2.mass);
-  ball2.applyForce(gravity);
-  if (mouseIsPressed) {
-    ball.applyForce(wind);
-    ball2.applyForce(wind);
-  }
-  ball.update();
-  ball2.update();
-  ball.edgeBounce();
-  ball2.edgeBounce();
-  background('salmon');
-  Fill('white');
-  ball.display();
-  ball2.display();
-}
+  background(255);
 
-//모든 물체는 중력이 같은 게 아니라 중력 가속도가 같은 거다.
+  mover.update();
+  mover.checkEdges();
+  mover.display();
+  mover.displayVectors();
+}
