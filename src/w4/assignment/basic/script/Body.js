eba13a2fcbf1100e0cbaffb9d97215979a5b8f46
parksion0629 <143991677+parksion0629@users.noreply.github.com>
2023-10-16 11:14:28 +0900

    4주차 과제, 5주차 강의

---
+++ src/w4/assignment/basic/script/Body.js
@@ -0,0 +1,35 @@
+class Body {
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.mass = mass;
+    this.rad = this.mass ** (1 / 2) * 4;
+  }
+
+  attract(body) {
+    const force = p5.Vector.sub(this.pos, body.pos);
+    let distance = constrain(force.mag(), 80, 80);
+    let strength = (G * (this.mass * body.mass)) / distance ** 2;
+    force.setMag(strength);
+    return force;
+  }
+
+  applyForce(force) {
+    const acc = p5.Vector.div(force, this.mass);
+    this.acc.add(acc);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  display() {
+    noStroke();
+    strokeWeight(0.5);
+    fill(10, 127);
+    circle(this.pos.x, this.pos.y, this.rad * 2);
+  }
+}
--- src/w4/assignment/basic/script/sketch.js
+++ src/w4/assignment/basic/script/sketch.js
@@ -1,62 +1,30 @@
-let pos;
-let vel;
-let acc;
+let bodies = [];
+let bodyNum = 30;
+let G = 1;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  background('salmon');
-  pos = createVector(width / 2, height / 2);
-  vel = createVector(0, 0);
-  acc = createVector(0, 0);
+  reset();
+  background(255);
 }
 
 function draw() {
-  background(220);
-  let randomAcc = p5.Vector.random2D();
-  randomAcc.mult(0.2);
+  background(255);
 
-  // 현재 가속도에 더하기
-  acc.add(randomAcc);
+  for (let a = 0; a < bodyNum; a++) {
+    for (let j = 0; j < bodyNum; j++)
+      if (a !== j) bodies[j].applyForce(bodies[a].attract(bodies[j]));
 
-  // 현재 속도에 가속도 더하기
-  vel.add(acc);
-
-  // 속도 벡터 크기 제한
-  vel.limit(5); // 속도 상한선
-
-  // 위치 벡터에 속도 벡터 더하기
-  pos.add(vel);
-
-  // 화면 밖으로 나가지 않도록 위치 조정
-  if (pos.x > width) {
-    pos.x = 0;
-  } else if (pos.x < 0) {
-    pos.x = width;
+    bodies[a].update();
+    bodies[a].display();
   }
-  if (pos.y > height) {
-    pos.y = 0;
-  } else if (pos.y < 0) {
-    pos.y = height;
-  }
+}
 
-  // 원 그리기
-  noFill();
-  stroke(0);
-  ellipse(pos.x, pos.y, 50, 50);
+function mousePressed() {
+  if (isMouseInsideCanvas()) reset();
+}
 
-  // 중심에서 마우스로 향하는 벡터 그리기
-  let centerToMouse = createVector(mouseX - pos.x, mouseY - pos.y);
-  centerToMouse.mult(10); // 벡터 확대
-  stroke(255, 0, 0);
-  line(pos.x, pos.y, pos.x + centerToMouse.x, pos.y + centerToMouse.y);
-
-  // 가속도 벡터 그리기
-  acc.mult(100); // 벡터 확대
-  stroke(0, 0, 255);
-  line(pos.x, pos.y, pos.x + acc.x, pos.y + acc.y);
-
-  // 속도 벡터 그리기
-  vel.mult(10); // 벡터 확대
-  stroke(0, 255, 0);
-  line(pos.x, pos.y, pos.x + vel.x, pos.y + vel.y);
+function reset() {
+  for (let a = 0; a < bodyNum; a++)
+    bodies[a] = new Body(random(width), random(height), random(16, 100));
 }
